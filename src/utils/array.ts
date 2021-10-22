575c4b302563637addad23055383e8ecb245351d
Mortea Ebrahimi <mz.ebrahimi@gmail.com>
2021-10-21 23:25:04 -0400

    Remove unused files
    Remove unused files
    
    Remove unused files

--- src/utils/array.ts
+++
@@ -1,8 +0,0 @@
-export function arrayEquals(a: Array<any>, b: Array<any>) {
-    return (
-        Array.isArray(a) &&
-        Array.isArray(b) &&
-        a.length === b.length &&
-        a.every((val, index) => val === b[index])
-    );
-}
